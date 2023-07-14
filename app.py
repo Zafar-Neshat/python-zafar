03fadbaf1682a9cf0b02a8ee2239980569ed3a9e
zafar neshat <zafarnishat1381@gmail.com>
2023-07-14 19:01:13 +0430

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# H.W
+
+if 80 == 80:
+    print("same")
+
+if 40 == 40:
+    print("equal")
+
+if 150 < 100:
+    print("true")
+
+if 100 < 75:
+    print("100 is smaller than 70")
+
+if 500 > 200:
+    print("True")       
+
+if 80 > 45:
+    print("80 is bigger than 45")
+
---
+++ fanction.py
@@ -0,0 +1,29 @@
+# H.W
+
+def multipy():
+    x = 30
+    y = 50
+    print(x * y)
+
+multipy()
+
+def divide():
+    x = 15000
+    y = 200
+    print(x / y)
+
+divide()
+
+def min():
+    x = 500
+    y = 150
+    print(x - y)
+
+min()
+
+def sum():
+    x = 500
+    y = 120
+    print(x + y)
+
+sum()
